2fb46975d80532e67fff0335a4a7ff8cd926d1f1
ealemucrls <26ealemu@cpsd.us>
2025-06-16 00:26:39 -0400

    Creating the operate function
    the operate function checks to see if that what the user wants to do with the numbers it's inputted. Then a string operator is casted to all lowercase letters to avoid any errors, and then it's just if and else statements check to which operator was chosen.

--- calculator.js
+++ calculator.js
@@ -18,6 +18,24 @@ function division(a, b){
     return sum; 
 }
 
-function operate(operator, a, b){
 
+function operate(operator, a, b){
+    let operator = operator.toLowerCase();
+    if(operator.equals("add")){
+        add(a, b);
+    }
+    else if(operator.equals("subtract")){
+        subtract(a,b);
+    }
+    else if(operator.equals("multiply")){
+        multiply(a,b);
+    }
+    else if(operator.equals("division")){
+        division(a,b);
+    }
 }
+    
+
+   
+
+
