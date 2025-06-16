fc989d60059bc54208f08787e5783e2b8d61f025
ealemucrls <26ealemu@cpsd.us>
2025-06-16 00:04:33 -0400

    Basic calculator functions
    I created basic calculator functions that do addition, subtraction, multiplication, and division.

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    let sum = a + b;
+    return sum; 
+}
+
+function subtract(c,d){
+    let sum = c - d;
+    return sum;
+}
+
+function multiply(e,f){
+    let sum = f * e;
+    return sum;
+}
+
+function division(g, h){
+    let sum = g / h;
+    return sum; 
+}
